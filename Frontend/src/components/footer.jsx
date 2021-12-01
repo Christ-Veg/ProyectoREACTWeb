b10614861bbf7780d02f6a472d0d1f45f79c31be
Emanuel Vega <christian990116@gmail.com>
2021-12-01 13:24:46 -0600

    Se encuentra bug, HELP

---
+++ Frontend/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <span>Pagina Elaboradada por: Christian Vega </span>
+            <span><a href="https://www.linkedin.com/in/christian-emanuel-vega-bautista-896880173" target="_blank"><i class="fab fa-linkedin-in"></i></a></span>
+            <span><a href="https://github.com/Christ-Veg" target="_blank"><i class="fab fa-github"></i></a></span>
+        </footer>
+            
+
+    );
+}
---
+++ Frontend/src/routes/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '@styles/global.css';
+
+const App = () => {
+    return(
+        <Layout>
+            <Login />
+        </Layout>
+    );
+}
+
+export default App;

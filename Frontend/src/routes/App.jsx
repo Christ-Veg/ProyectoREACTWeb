b4c7739d29b422524f0c70faabc93b1f49f9a4c2
Emanuel Vega <christian990116@gmail.com>
2021-12-02 00:44:10 -0600

    Se comenzará de nuevo las rutas para login

--- Frontend/src/routes/App.jsx
+++ Frontend/src/routes/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import '@styles/global.css';
 import Layout from "@containers/Layout";
 import Login from "@pages/login";
-import Footer from "@components/footer";
+
 
 const App = () => {
     return(
